051eb7c8318b306314fc04dc34549b6686c91f12
Herminio Torres <herminiocesar@gmail.com>
2020-04-05 23:35:58 -0300

    feat: refactor the dictionary

--- game/dictionary/lib/dictionary.ex
+++ game/dictionary/lib/dictionary.ex
@@ -1,13 +1,6 @@
 defmodule Dictionary do
-  def random_word() do
-    word_list()
-    |> Enum.random()
-  end
+  alias Dictionary.WordList
 
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+  defdelegate start(), to: WordList, as: :word_list
+  defdelegate random_word(word_list), to: WordList
 end
