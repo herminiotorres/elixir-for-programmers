33954a68484c405ee4a15b377697b8a051690cb2
Herminio Torres <herminiocesar@gmail.com>
2020-04-10 16:38:31 -0300

    feat: no, mr dictionary, I expect you to die

--- game/dictionary/lib/dictionary/application.ex
+++ game/dictionary/lib/dictionary/application.ex
@@ -2,6 +2,20 @@ defmodule Dictionary.Application do
   use Application
 
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    import Supervisor.Spec, warn: false
+
+    children = [
+      %{
+        id: Dictionary.WordList,
+        start: {Dictionary.WordList, :start_link, []}
+      }
+    ]
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 end
